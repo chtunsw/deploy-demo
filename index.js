84618757f50d13d81322e5f3d2fd20c70df368bf
chen <chtunsw@gmail.com>
2019-05-19 12:21:04 +1000

    finished

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res, next) => {
-  res.send("Hello World! AWS ECS!");
+  res.send("Hello World! New AWS ECS!");
 })
 
 app.listen(3000, () => {
